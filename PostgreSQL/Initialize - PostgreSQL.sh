7374cfdc740699fe64f88b5a87a1709ac2a14acd
Maxim Moinat <maxim@thehyve.nl>
2017-10-13 10:12:05 +0200

    initialize script now also accepts hostname, port and password

--- PostgreSQL/Initialize - PostgreSQL.sh
+++ PostgreSQL/Initialize - PostgreSQL.sh
@@ -6,17 +6,19 @@
 # Example usage: sh execute.sh omops cdm5_2_1 postgres /Documents/OHDSI/OMOP_Data/vocab_download_v5_meddra/ /Documents/OHDSI/OMOP_Data/synpuf_1k/
 
 # Variables
-DATABASE_NAME="$1"
-DATABASE_SCHEMA="$2"
-USER="$3"
-VOCABULARY_PATH="$4" # Absolute path
-CDM_DATA_PATH="$5"
-CDM_DATA_FILE_PREFIX="$6"
+HOST_NAME="$1"
+PORT="$2"
+DATABASE_NAME="$3"
+DATABASE_SCHEMA="$4"
+USER="$5"
+PASSWORD="$6"
+VOCABULARY_PATH="$7" # Absolute path
+CDM_DATA_PATH="$8"
 
 # Check whether command line arguments are given
 if [[ $DATABASE_NAME = "" ]] || [[ $USER = "" ]] || [[ $DATABASE_SCHEMA = "" ]]; then
-    printf "Please input a database name, schema name and username: \n"
-    printf "   ./execute_etl.sh <database_name> <schema_name> <user_name> [<vocabulary_path>] [<cdm_data_path>]\n"
+    printf "Usage: \n"
+    printf "   ./execute_etl.sh <host> <port> <database_name> <schema_name> <user_name> <password> [<vocabulary_path> [<cdm_data_path>]]\n"
     exit 1
 fi
 
@@ -24,13 +26,15 @@ printf "===== Starting Create OMOP CDM instance =====\n"
 printf "Using the database '$DATABASE_NAME' and the '$DATABASE_SCHEMA' schema.\n"
 printf "Creating OMOP CDM, load vocabulary from '$VOCABULARY_PATH' and load CDM data form '$CDM_DATA_PATH'.\n"
 
+export PGPASSWORD=$PASSWORD
+
 # Create new schema and set it als default schema (no schema specified in loading scripts)
-sudo -u $USER psql -d $DATABASE_NAME -c "DROP SCHEMA IF EXISTS $DATABASE_SCHEMA CASCADE;"
-sudo -u $USER psql -d $DATABASE_NAME -c "CREATE SCHEMA $DATABASE_SCHEMA;"
-sudo -u $USER psql -d $DATABASE_NAME -c "ALTER DATABASE $DATABASE_NAME SET search_path TO $DATABASE_SCHEMA, public;"
+psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -c "DROP SCHEMA IF EXISTS $DATABASE_SCHEMA CASCADE;"
+psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -c "CREATE SCHEMA $DATABASE_SCHEMA;"
+psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -c "ALTER DATABASE $DATABASE_NAME SET search_path TO $DATABASE_SCHEMA, public;"
 
 # Create cdm tables
-sudo -u $USER psql -d $DATABASE_NAME -f "./OMOP CDM ddl - PostgreSQL.sql" -q
+psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -f "./OMOP CDM ddl - PostgreSQL.sql" -q
 
 # Load vocabulary
 if [[ $VOCABULARY_PATH != "" ]]; then
@@ -39,7 +43,7 @@ if [[ $VOCABULARY_PATH != "" ]]; then
     for tableName in "${vocab_tables[@]}"; do :
         printf "$tableName: "
         path="$VOCABULARY_PATH/$tableName.csv"
-        sudo -u $USER psql -d $DATABASE_NAME -c "COPY $tableName FROM '$path' WITH DELIMITER E'\t' CSV HEADER QUOTE E'\b'"
+        psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -c "COPY $tableName FROM '$path' WITH DELIMITER E'\t' CSV HEADER QUOTE E'\b'"
     done
 fi
 
@@ -49,15 +53,15 @@ if [[ $CDM_DATA_PATH != "" ]]; then
     cdm_tables=(CARE_SITE CONDITION_OCCURRENCE DEATH DRUG_EXPOSURE DEVICE_EXPOSURE LOCATION MEASUREMENT OBSERVATION PERSON PROCEDURE_OCCURRENCE PROVIDER VISIT_OCCURRENCE DRUG_ERA CONDITION_ERA)
     for tableName in "${cdm_tables[@]}"; do :
         printf "$tableName: "
-        path="$CDM_DATA_PATH/$CDM_DATA_FILE_PREFIX$tableName.csv"
-        sudo -u $USER psql -d $DATABASE_NAME -c "COPY $tableName FROM '$path' WITH DELIMITER E',' CSV HEADER QUOTE E'\b'"
+        path="$CDM_DATA_PATH/$tableName.csv"
+        psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -c "COPY $tableName FROM '$path' WITH DELIMITER E',' CSV HEADER QUOTE E'\b'"
     done
 fi
 
 printf "\nApplying constraints...\n"
-sudo -u $USER psql -d $DATABASE_NAME -f "./OMOP CDM constraints - PostgreSQL.sql" -q
+psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -f "./OMOP CDM constraints - PostgreSQL.sql" -q
 printf "\nApplying indices...\n"
-sudo -u $USER psql -d $DATABASE_NAME -f "./OMOP CDM indexes required - PostgreSQL.sql"
+psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -f "./OMOP CDM indexes required - PostgreSQL.sql"
 
 # Restore search path
-sudo -u $USER psql -d $DATABASE_NAME -c "ALTER DATABASE $DATABASE_NAME SET search_path TO \"\$user\", public;"
+psql -h $HOST_NAME -p $PORT -U $USER -d $DATABASE_NAME -c "ALTER DATABASE $DATABASE_NAME SET search_path TO \"\$user\", public;"
